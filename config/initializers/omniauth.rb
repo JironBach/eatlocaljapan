929bc9888b84dd9907cdae777001256f07bd3502
JironBach <junshimo2@gmail.com>
2016-03-15 19:43:37 +0900

    add omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  #provider :facebook, '1068770586487027', '21daca4e576b8a2840c6bf5508df30be',
+  #  scope: 'email,public_profile,user_about_me,user_birthday',
+  #  info_fields: 'id,name,email,gender,link,locale,name,verified,birthday,cover',
+  #  strategy_class: OmniAuth::Strategies::Facebook,
+  #  provider_ignores_state: true
+end
