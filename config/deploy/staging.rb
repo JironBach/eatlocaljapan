bb7d260cc6fd3d174d0206c483666d4a102c8825
JironBach <junshimo2@gmail.com>
2016-04-03 10:19:11 +0900

    try to deploy production.

---
+++ config/deploy/staging.rb
@@ -0,0 +1,25 @@
+set :stage, :staging #環境名
+
+# server-based syntax
+# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
+#server 'git.mydns.jp', user: 'js', roles: %w{app db web}
+
+#各サーバの役割を記述
+role :app, %w{js@www.eatlocaljapan}#, my_property: :my_value
+role :web, %w{js@www.eatlocaljapan.com}#, other_property: :other_value
+role :db,  %w{js@www.eatlocaljapan.com}
+
+#サーバ情報記述
+server 'www.eatlocaljapan.com',
+user: 'eatlocaljapan',
+roles: %w{web app db},
+  ssh_options: {
+    port: 22,
+    user: 'eatlocaljapan',
+    keys: %w(/Users/js/.ssh/eatlocaljapan.key),
+    forward_agent: true,
+    auth_methods: %w(publickey password)
+    # password: 'please use keys'
+  }
