409927b1b98a1e5f58821e582acf003197b8c449
JironBach <junshimo2@gmail.com>
2016-04-06 17:39:46 +0900

    migration review.

--- db/migrate/20160406075917_add_columns_to_listing.rb
+++ db/migrate/20160406075917_add_columns_to_listing.rb
@@ -1,21 +1,18 @@
 class AddColumnsToListing < ActiveRecord::Migration
   def change
     add_column :listing, :smoking_id, :integer
-    add_column :listing, :english_support, :integer
-    add_column :listing, :business_hours, :string
-    add_column :listing, :restaurant_ja, :string
-    add_column :listing, :restaurant_en, :string
-    add_column :listing, :menu_ja, :string
-    add_column :listing, :menu_en, :string
+    add_column :listing, :english_id, :integer
+    add_column :listing, :business_hours_id, :string
+    add_column :listing, :description, :text
+    add_column :listing, :description_en, :text
     add_column :listing, :price_low, :integer
     add_column :listing, :price_high, :integer
-    add_column :listing, :address_text, :string
-    add_column :listing, :address_map, :string
+    add_column :listing, :address, :string
     add_column :listing, :tel, :string
-    add_column :listing, :website_url, :string
-    add_column :listing, :reviewsite_url, :string
-    add_column :listing, :recommended_ja, :string
-    add_column :listing, :recommended_en, :string
+    add_column :listing, :url, :string
+    add_column :listing, :review_url, :string
+    add_column :listing, :recommended, :text
+    add_column :listing, :recommended_en, :text
     add_column :listing, :visit_benefits, :string
     add_column :listing, :visit_benefits_another, :string
   end
