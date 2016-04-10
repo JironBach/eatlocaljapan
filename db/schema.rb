f7597c6c4b4825743327357923d62518ae8bb633
JironBach <junshimo2@gmail.com>
2016-04-10 21:30:48 +0900

    店舗表示の改変 。

--- db/schema.rb
+++ db/schema.rb
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 20160406075917) do
     t.string   "cover_image_caption",                            default: ""
     t.datetime "created_at",                                                     null: false
     t.datetime "updated_at",                                                     null: false
+    t.string   "address"
     t.integer  "smoking_id"
     t.integer  "english_id"
     t.string   "business_hours_id"
@@ -203,7 +204,6 @@ ActiveRecord::Schema.define(version: 20160406075917) do
     t.text     "shop_description_en"
     t.integer  "price_low"
     t.integer  "price_high"
-    t.string   "address"
     t.string   "tel"
     t.string   "url"
     t.string   "review_url"
