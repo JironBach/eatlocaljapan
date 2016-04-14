cad7900703720bff069258208e084b1d2ee40487
Tomohiro Matsushima <mattomo73@gmail.com>
2016-04-14 18:06:36 +0900

    喫煙・禁煙設定

---
+++ db/fixtures/development/smoking.rb
@@ -0,0 +1,6 @@
+Smoking.seed(
+  { id: 1, name: '完全禁煙', name_en: 'No smoking' },
+  { id: 2, name: '分煙（エリアによる分煙）', name_en: 'Separation of smoking areas by area' },
+  { id: 3, name: '完全分煙（部屋による分煙）', name_en: 'Separation of smoking areas by room' },
+  { id: 4, name: '喫煙可', name_en: 'Smoking' },
+)
