e4284fe4060a0427524ea80c3e1501b6903e2ce6
JironBach <junshimo2@gmail.com>
2016-05-09 16:36:58 +0900

    merge from develop.

---
+++ db/fixtures/production/smoking.rb
@@ -0,0 +1,6 @@
+Smoking.seed(
+  { id: 1, name: '完全禁煙', name_en: 'Non-smoking' },
+  { id: 2, name: '分煙（エリアによる分煙）', name_en: 'Smoking in separated area' },
+  { id: 3, name: '完全分煙（部屋による分煙）', name_en: 'Smoking in separated rooms' },
+  { id: 4, name: '喫煙可', name_en: 'Smoking is permitted' },
+)
