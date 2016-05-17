731a2105c3cdaf95e9e9382f0c36cbd4894c8b73
JironBach <junshimo2@gmail.com>
2016-05-17 17:10:38 +0900

    詳細と管理画面の一致。

--- app/admin/listing.rb
+++ app/admin/listing.rb
@@ -13,7 +13,7 @@ ActiveAdmin.register Listing do
 #   permitted
 # end
 
-  permit_params :user_id, :open, :ave_total, :ave_accuracy, :ave_communication, :ave_cleanliness, :ave_location, :ave_check_in, :ave_cost_performance, :open, :zipcode, :location, :longitude, :latitude, :delivery_flg, :price, :description, :title, :capacity, :direction, :cover_image_caption, :business_hours_remarks, :shop_description, :shop_description_en, :price_low, :price_high, :tel, :url, :review_url, :recommended, :recommended_en, :visit_benefits, :visit_benefits_another, :smoking_id,
+  permit_params :user_id, :open, :ave_total, :ave_accuracy, :ave_communication, :ave_cleanliness, :ave_location, :ave_check_in, :ave_cost_performance, :open, :zipcode, :location, :longitude, :latitude, :delivery_flg, :price, :description, :title, :title_en, :capacity, :direction, :cover_image_caption, :business_hours_remarks, :shop_description, :shop_description_en, :price_low, :price_high, :tel, :url, :review_url, :recommended, :recommended_en, :visit_benefits, :visit_benefits_another, :smoking_id,
     english_ids: [], shop_category_ids: [], shop_service_ids: []
 
   index do
@@ -22,6 +22,7 @@ ActiveAdmin.register Listing do
       link_to id.id, admin_listing_path(id)
     end
     column I18n.t('activerecord.attributes.listing.title'), :title
+    column I18n.t('activerecord.attributes.listing.title_en'), :title_en
     column I18n.t('activerecord.attributes.listing.open'), :open
 
     column I18n.t('activerecord.attributes.listing.shop_categories') do |sc|
@@ -30,11 +31,11 @@ ActiveAdmin.register Listing do
     column I18n.t('activerecord.attributes.listing.shop_services') do |ss|
       ShopService.where(id: ss.shop_services).all.pluck(:name).join(', ')
     end
-    column I18n.t('activerecord.attributes.listing.description'), :description
+    #column I18n.t('activerecord.attributes.listing.description'), :description
     column I18n.t('activerecord.attributes.listing.cover_image'), :cover_image
     column I18n.t('activerecord.attributes.listing.zipcode'), :zipcode
     column I18n.t('activerecord.attributes.listing.location'), :location
-    column I18n.t('activerecord.attributes.listing.direction'), :direction
+    #column I18n.t('activerecord.attributes.listing.direction'), :direction
     column I18n.t('activerecord.attributes.listing.smoking_id'), sortable: :smoking_id do |s|
       Smoking.where(id: s.smoking_id).all.pluck(:name).join(', ')
     end
@@ -42,11 +43,13 @@ ActiveAdmin.register Listing do
       English.where(id: e.englishes).all.pluck(:name).join(', ')
     end
     # business_hour
-    column I18n.t('activerecord.attributes.listing.business_hours_remarks'), :business_hours_remarks
+    #column I18n.t('activerecord.attributes.listing.business_hours_remarks'), :business_hours_remarks
     column I18n.t('activerecord.attributes.listing.shop_description'), :shop_description
     column I18n.t('activerecord.attributes.listing.shop_description_en'), :shop_description_en
     column I18n.t('activerecord.attributes.listing.price_low'), :price_low
     column I18n.t('activerecord.attributes.listing.price_high'), :price_high
+    column I18n.t('activerecord.attributes.listing.price_low_dinner'), :price_low_dinner
+    column I18n.t('activerecord.attributes.listing.price_high_dinner'), :price_high_dinner
     column I18n.t('activerecord.attributes.listing.tel'), :tel
     column I18n.t('activerecord.attributes.listing.url'), :url
     column I18n.t('activerecord.attributes.listing.review_url'), :review_url
@@ -54,29 +57,34 @@ ActiveAdmin.register Listing do
     column I18n.t('activerecord.attributes.listing.recommended_en'), :recommended_en
     column I18n.t('activerecord.attributes.listing.visit_benefits'), :visit_benefits
     column I18n.t('activerecord.attributes.listing.visit_benefits_another'), :visit_benefits_another
+    column I18n.t('activerecord.attributes.listing.english_messages'), :english_messages
+    column I18n.t('activerecord.attributes.listing.info_admin'), :info_admin
     actions
   end
 
   form do |f|
     f.inputs "店舗情報の詳細" do
       f.input :title
+      f.input :title_en
       f.input :open
 
       f.input :shop_categories, :as => :check_boxes, collection: ShopCategory.all
       f.input :shop_services, :as => :check_boxes, collection: ShopService.all
-      f.input :description
+      #f.input :description
       f.input :cover_image
       f.input :zipcode
       f.input :location
-      f.input :direction
+      #f.input :direction
       f.input :smoking_id, :as => :select, collection: Smoking.all
       f.input :englishes, :as => :check_boxes, collection: English.all
       # business_hour
-      f.input :business_hours_remarks
+      #f.input :business_hours_remarks
       f.input :shop_description
       f.input :shop_description_en
       f.input :price_low
       f.input :price_high
+      f.input :price_low_dinner
+      f.input :price_high_dinner
       f.input :tel
       f.input :url
       f.input :review_url
@@ -84,6 +92,8 @@ ActiveAdmin.register Listing do
       f.input :recommended_en
       f.input :visit_benefits
       f.input :visit_benefits_another
+      f.input :english_messages
+      f.input :info_admin_id
     end
     f.actions
   end
@@ -92,6 +102,9 @@ ActiveAdmin.register Listing do
     attributes_table do
       row I18n.t('activerecord.attributes.listing.title') do
         resource.title
+      end
+      row I18n.t('activerecord.attributes.listing.title_en') do
+        resource.title_en
       end
       row I18n.t('activerecord.attributes.listing.open') do
         resource.open
@@ -102,9 +115,9 @@ ActiveAdmin.register Listing do
       row I18n.t('activerecord.attributes.listing.shop_services') do
         ShopService.where(id: resource.shop_services).all.pluck(:name).join(', ')
       end
-      row I18n.t('activerecord.attributes.listing.description') do
-        resource.description
-      end
+      #row I18n.t('activerecord.attributes.listing.description') do
+      #  resource.description
+      #end
       row I18n.t('activerecord.attributes.listing.cover_image') do
         resource.cover_image
       end
@@ -114,9 +127,9 @@ ActiveAdmin.register Listing do
       row I18n.t('activerecord.attributes.listing.location') do
         resource.location
       end
-      row I18n.t('activerecord.attributes.listing.direction') do
-        resource.direction
-      end
+      #row I18n.t('activerecord.attributes.listing.direction') do
+      #  resource.direction
+      #end
       row I18n.t('activerecord.attributes.listing.smoking_id') do
         Smoking.where(id: resource.smoking_id).all.pluck(:name).join(', ')
       end
@@ -124,9 +137,9 @@ ActiveAdmin.register Listing do
         English.where(id: resource.englishes).all.pluck(:name).join(', ')
       end
       # business_hour
-      row I18n.t('activerecord.attributes.listing.business_hours_remarks') do
-        resource.business_hours_remarks
-      end
+      #row I18n.t('activerecord.attributes.listing.business_hours_remarks') do
+      #  resource.business_hours_remarks
+      #end
       row I18n.t('activerecord.attributes.listing.shop_description') do
         resource.shop_description
       end
@@ -138,6 +151,12 @@ ActiveAdmin.register Listing do
       end
       row I18n.t('activerecord.attributes.listing.price_high') do
         resource.price_high
+      end
+      row I18n.t('activerecord.attributes.listing.price_low_dinner') do
+        resource.price_low_dinner
+      end
+      row I18n.t('activerecord.attributes.listing.price_high_dinner') do
+        resource.price_high_dinner
       end
       row I18n.t('activerecord.attributes.listing.tel') do
         resource.tel
@@ -159,6 +178,12 @@ ActiveAdmin.register Listing do
       end
       row I18n.t('activerecord.attributes.listing.visit_benefits_another') do
         resource.visit_benefits_another
+      end
+      row I18n.t('activerecord.attributes.listing.english_message') do
+        resource.english_messages
+      end
+      row I18n.t('activerecord.attributes.listing.info_admin') do
+        resource.info_admin
       end
     end
   end
