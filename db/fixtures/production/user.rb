2822fea1fa0904fda7e32ce8d6d1ab3ca9494223
JironBach <junshimo2@gmail.com>
2016-05-23 16:34:47 +0900

    seed for staging.

---
+++ db/fixtures/production/user.rb
@@ -0,0 +1,4 @@
+User.seed(
+  { id: 1, email: 'junshimo2@gmail.com', password: 'airjun51bnb', password_confirmation: 'airjun51bnb', admin: true, confirmed_at: Time.now },
+  { id: 2, email: 'chisa.odamaki@bizitjapan.com', password: 'airco55bnb', password_confirmation: 'airco55bnb', admin: true, confirmed_at: Time.now },
+)
