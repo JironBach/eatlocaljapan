e9b9837300e524406c3fab86c476c02b27474678
JironBach <junshimo2@gmail.com>
2016-05-23 17:54:04 +0900

    seed見直し。

--- db/fixtures/production/user.rb
+++ db/fixtures/production/user.rb
@@ -1,4 +1,4 @@
 User.seed(
-  { id: 1, email: 'junshimo2@gmail.com', password: 'airjun51bnb', password_confirmation: 'airjun51bnb', admin: true, confirmed_at: Time.now },
-  { id: 2, email: 'chisa.odamaki@bizitjapan.com', password: 'airco55bnb', password_confirmation: 'airco55bnb', admin: true, confirmed_at: Time.now },
+  { id: 1, email: 'jironbach@gmail.com', password: 'bizit51japan', password_confirmation: 'airjun51bnb', admin: true, confirmed_at: Time.now },
+  { id: 2, email: 'bizitjapan@gmail.com', password: 'bizit55japan', password_confirmation: 'airjun51bnb', admin: true, confirmed_at: Time.now },
 )
