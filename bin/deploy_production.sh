3711fb4847807470a4bc41df85b9f2030048514e
JironBach <junshimo2@gmail.com>
2016-05-25 13:06:57 +0900

    ログインしてなくても店舗詳細を見られるように。

--- bin/deploy_production.sh
+++ bin/deploy_production.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 scp -r -P 12321 eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan//shared/public/* public_production
-scp -r -P 12321 bin/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/bin
 scp -r -P 12321 public_production/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public
+scp -r -P 12321 bin/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/bin
 chmod -R a+r app/assets
 scp -r -P 12321 app/assets/*/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public/assets
 cp config/deploy_production.rb config/deploy.rb
--- bin/deploy_staging.sh
+++ bin/deploy_staging.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 scp -r -P 12321 js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public/* public_staging
-scp -r -P 12321 bin/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/bin
 scp -r -P 12321 public_staging/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public
+scp -r -P 12321 bin/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/bin
 chmod -R a+r app/assets
 scp -r -P 12321 app/assets/*/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public/assets
 cp config/deploy_staging.rb config/deploy.rb
