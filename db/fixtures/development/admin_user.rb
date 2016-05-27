b515ec1fda7f81153c02322f93a3592368798dd0
JironBach <junshimo2@gmail.com>
2016-05-27 21:30:04 +0900

    admin_userを空に。

--- db/fixtures/development/admin_user.rb
+++ db/fixtures/development/admin_user.rb
@@ -1,4 +1,6 @@
+=begin
 AdminUser.seed(
   { id: 1, email: 'junshimo2@gmail.com', password: 'airjun51bnb', password_confirmation: 'airjun51bnb' },
   { id: 2, email: 'chisa.odamaki@bizitjapan.com', password: 'airco55bnb', password_confirmation: 'airco55bnb' },
 )
+=end
