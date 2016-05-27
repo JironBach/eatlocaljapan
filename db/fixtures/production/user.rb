c5fd1722ede637761848581d74f86dad49084bc7
JironBach <junshimo2@gmail.com>
2016-05-27 21:45:21 +0900

    小田巻さんが登録できない。

--- db/fixtures/production/user.rb
+++ db/fixtures/production/user.rb
@@ -1,4 +1,4 @@
 User.seed(
   { id: 1, email: 'jironbach@gmail.com', password: 'bizit51japan', password_confirmation: 'bizit51japan', admin: true, confirmed_at: Time.now },
-  { id: 2, email: 'bizitjapan@gmail.com', password: 'bizit55japan', password_confirmation: 'bizit55japan', admin: true, confirmed_at: Time.now },
+  { id: 2, email: 'chisa.odamaki@bizitjapan.com', password: 'bizit55japan', password_confirmation: 'bizit55japan', admin: true, confirmed_at: Time.now },
 )
