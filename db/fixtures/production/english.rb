a81e57d01f852a14554785db912b342d321e8ffa
JironBach <junshimo2@gmail.com>
2016-05-31 10:03:05 +0900

    英語表記修正。

--- db/fixtures/production/english.rb
+++ db/fixtures/production/english.rb
@@ -1,5 +1,5 @@
 English.seed(
   { id: 1, name: '英語メニューあり', name_en: 'English menu' },
   { id: 2, name: '英語できるスタッフいます', name_en: 'English speaking staff' },
-  { id: 3, name: '日本語スタッフだけですが心より歓迎いたします', name_en: 'Japanese speaking staff only but we''d be delighted to welcome all our guests!' },
+  { id: 3, name: '日本語スタッフだけですが心より歓迎いたします', name_en: "Japanese speaking staff only but we'd be delighted to welcome all our guests!" },
 )
