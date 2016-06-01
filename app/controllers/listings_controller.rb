058cc5b40143a398ec13bad9d05d2c22b0880f7f
JironBach <junshimo2@gmail.com>
2016-06-01 20:11:54 +0900

    翻訳debug.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -60,7 +60,8 @@ class ListingsController < ApplicationController
     #{EasyTranslate.translate(@listing.description, to: :en)}" if @listing.description_en.blank?
     @listing.location_en = "(Translated by Google)
     #{EasyTranslate.translate(@listing.location, to: :en)}" if @listing.location_en.blank?
-    @listing.recommended_en = EasyTranslate.translate(@listing.recommended, to: :en) if @listing.recommended_en.blank?
+    @listing.recommended_en = "(Translated by Google)
+    #{EasyTranslate.translate(@listing.recommended, to: :en)}" if @listing.recommended_en.blank?
     @listing.visit_benefits_en = "(Translated by Google)
     #{EasyTranslate.translate(@listing.visit_benefits, to: :en)}" if @listing.visit_benefits_en.blank?
     @listing.visit_benefits_another_en = "(Translated by Google)
