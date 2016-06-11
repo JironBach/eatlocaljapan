7fde68eb6e5e80eb7dcd18d5dc7b85f4d869110d
JironBach <junshimo2@gmail.com>
2016-06-11 16:48:39 +0900

    adminユーザー追加。

--- db/fixtures/production/admin_user.rb
+++ db/fixtures/production/admin_user.rb
@@ -1,6 +1,4 @@
-=begin
 AdminUser.seed(
-  { id: 1, email: 'junshimo2@gmail.com', password: 'airjun51bnb', password_confirmation: 'airjun51bnb' },
-  { id: 2, email: 'chisa.odamaki@bizitjapan.com', password: 'airco55bnb', password_confirmation: 'airco55bnb' },
+  #{ id: 1, email: 'junshimo2@gmail.com', password: 'airjun51bnb', password_confirmation: 'airjun51bnb' },
+  { id: 1, email: 'chisa.odamaki@bizitjapan.com', password: 'airco55bnb', password_confirmation: 'airco55bnb' },
 )
-=end
