a3f4eae40456aa280a41b8688a2f27421a721286
JironBach <junshimo2@gmail.com>
2016-06-13 00:21:22 +0900

    backupプロセス見直し。@

--- bin/backup/backup_production.rb
+++ bin/backup/backup_production.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-#path = '/home/eatlocaljapan/www/production/eatlocaljapan/current'
-backup_to = "/home/eatlocaljapan/backup/#{Time.now.strftime("%j%H%M%S")}.prd"
+path = '/home/eatlocaljapan/backup'
+backup_to = "#{path}/#{Time.now.strftime("%j%H%M%S")}.prd"
 `/usr/bin/pg_dump -h www.eatlocaljapan.com -d eatlocaljapan -U eatlocaljapan -f #{backup_to}`
 
-bks = Dir.glob("#{backup_to}")
+bks = Dir.glob("#{path}")
 if bks.count > 10
   i = 0
   bks.reverse.each do |bk|
