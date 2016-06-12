8f6e319edc768888c7751ab4e48710b522078cb2
JironBach <junshimo2@gmail.com>
2016-06-13 00:02:32 +0900

    セッションが保存できるか確認。

--- bin/deploy_production.sh
+++ bin/deploy_production.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-scp -r eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan//shared/public/* public_production
-scp -r public_production/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public
+# 画像をバックアップ
+scp -r eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public
+# セッションの保存
+scp -r eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan//shared/tmp/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/tmp
+# binをアップロード
 scp -r bin/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/bin
 chmod -R a+r app/assets
+# assetsをアップロード
 scp -r app/assets/*/* eatlocaljapan@www.eatlocaljapan.com:~/www/production/eatlocaljapan/shared/public/assets
 cp config/deploy_production.rb config/deploy.rb
 cp Capfile_production Capfile
--- bin/deploy_staging.sh
+++ bin/deploy_staging.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-scp -r -P 12321 js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public/* public_staging
-scp -r -P 12321 public_staging/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public
+# 画像をバックアップ
+scp -r -P 12321 js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public
+# セッションの保存
+scp -r -P 12321 js@jironbach.iobb.net:~/www/production/eatlocaljapan/shared/tmp/* js@jironbach.iobb.net:~/www/production/eatlocaljapan/shared/tmp
+# binをアップロード
 scp -r -P 12321 bin/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/bin
 chmod -R a+r app/assets
 scp -r -P 12321 app/assets/*/* js@jironbach.iobb.net:~/www/staging/eatlocaljapan/shared/public/assets
