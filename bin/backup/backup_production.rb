e6caf1934df2244b06a3f6fddf1781df4de601a2
JironBach <junshimo2@gmail.com>
2016-06-13 13:11:47 +0900

    debug backup.

--- bin/backup/backup_production.rb
+++ bin/backup/backup_production.rb
@@ -5,7 +5,7 @@ path = '/home/eatlocaljapan/backup'
 backup_to = "#{path}/#{Time.now.strftime("%j%H%M%S")}.prd"
 `/usr/bin/pg_dump -h www.eatlocaljapan.com -d eatlocaljapan -U eatlocaljapan -f #{backup_to}`
 
-bks = Dir.glob("#{path}")
+bks = Dir.glob("#{path}/*.prd")
 if bks.count > 10
   i = 0
   bks.reverse.each do |bk|
