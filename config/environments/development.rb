9e9ad4575166d7ef712d3f72ece188db3d6a1e03
smoriwaki <smoriwaki@cuon.co.jp>
2016-11-09 21:12:44 +0900

    fix bug around mailer in configuration file for development. (refs #16914)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
   # Mailer
   config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.delivery_method = :sendmail
 
   config.action_controller.action_on_unpermitted_parameters = :raise
